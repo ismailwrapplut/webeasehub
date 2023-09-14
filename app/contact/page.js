ec9597d8e8863bd5ea7911997139a8364b106006
Mohd Ismail <smileweb.developer@gmail.com>
2023-09-14 19:52:02 +0530

    Dd

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Page = () => {
   return (
  <section className="text-gray-600 body-font relative">
-  <form data-netlify="true" className="container px-5 py-24 mx-auto">
+  <form data-netlify="true" className="container px-5 py-24 mx-auto" name="contact" method="POST">
     <div className="flex flex-col text-center w-full mb-12">
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Contact Us</h1>
     
